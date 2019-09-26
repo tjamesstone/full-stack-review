1e0a92366e799cad05e13a24ec644e3dd2d64e93
Tate Stone <tatejamesstone@gmail.com>
2019-09-26 10:59:23 -0600

    Routing

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
+import {Switch, Route} from 'react-router-dom'
 import React from 'react'
 import Catalog from './components/Catalog/Catalog'
 import Cart from './components/Cart/Cart'
 import Admin from './components/Admin/Admin'
-import {Switch, Route} from 'react-router-dom'
 
 export default(
     <Switch>
-        <Route exact pather ='/' component={Catalog}/>
+        <Route exact path ='/' component={Catalog}/>
         <Route path='/cart' component={Cart} />
         <Route path='/admin' component={Admin}/>
     </Switch>
