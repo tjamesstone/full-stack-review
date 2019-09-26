a2669bb61a55689b88218a8c80646ccc7c38e702
Tate Stone <tatejamesstone@gmail.com>
2019-09-26 15:26:57 -0600

    Redux work

---
+++ src/ducks/reducer.js
@@ -0,0 +1,20 @@
+//INITIAL STATE
+const initialState = {
+    loggedIn: false,
+    user: null
+}
+
+//ACTION CONTS
+
+
+//ACTION BUILDERS
+
+//REDUCER FUNC
+const reducer = (state = intitialState, action) => {
+    switch(action.type) {
+        default: return state
+    }
+}
+
+
+export default reducer
